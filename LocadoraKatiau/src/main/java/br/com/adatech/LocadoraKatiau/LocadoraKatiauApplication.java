2760cf0f2a2d8c1e06883a1afad211ca352f283c
Luuqee <www.luucas@gmail.com>
2024-11-01 11:24:00 -0300

    Organizando o Git

---
+++ LocadoraKatiau/src/main/java/br/com/adatech/LocadoraKatiau/LocadoraKatiauApplication.java
@@ -0,0 +1,13 @@
+package br.com.adatech.LocadoraKatiau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraKatiauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraKatiauApplication.class, args);
+	}
+
+}
---
+++ LocadoraKatiau/src/test/java/br/com/adatech/LocadoraKatiau/LocadoraKatiauApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adatech.LocadoraKatiau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraKatiauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
